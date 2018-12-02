d330c31ad853f7789715ee5bf5ce953fdd9755c3
SnesarevMS <snesarevms@sibintek.ru>
2018-12-02 16:46:13 +0300

    generating ports, countries and destinations

--- fill_db/gen_ports.py
+++ fill_db/gen_ports.py
@@ -3,30 +3,126 @@ from utils import *
 
 nav_db = '../resources/PUB151_distances.json'
 
-ships = "../db/ships.json"
-dests = "../db/dests.json"
+ports = "../resources/json/ports.json"
+dests = "../resources/json/dests.json"
+cntrs = "../resources/json/cntrs.json"
 
 
-def gen_ports_and_dests():
+def add_country(country, cntr_id, cntr_list):
+
+    cntr_sample = {
+        "_id"        : 0,
+        "name"       : "some_name"
+    }
+
+    in_cntr_list = False
+    for cntr in cntr_list:
+        if country == cntr["name"]:
+            in_cntr_list = True
+            break
+    if not in_cntr_list:
+        cntr = cntr_sample
+        cntr["_id"]  = cntr_id
+        cntr["name"] = country
+
+        cntr_list.append(cntr)
+
+        cntr_id += 1
+    return cntr_id, cntr_list
+
+
+def find_country(country, cntr_list):
+    for cntr in cntr_list:
+        if country == cntr["name"]:
+            return cntr["_id"]
+
+    return -1
+
+
+def find_port(name, port_list):
+    for port in port_list:
+        if name == port["name"]:
+            return port["_id"]
+
+    return -1
+
+
+def gen_ports():
 
     port_sample = {
+        "_id"        : 0,
         "name"       : "some_name",
         "country_id" : 0,
         "location"   : "27°05'37\"N., 13°26'55\"W."
     }
 
     dest_sample = {
+        "_id"        : 0,
         "departure"  : 0,
         "destination": 0,
         "distance"   : 0.0
     }
 
+    port_id = 0
+    dest_id = 0
+    cntr_id = 0
+
+    port_list = []
+    dest_list = []
+    cntr_list = []
+
     off_data = json_to_dict(nav_db)
 
-    for port_name, data in off_data.iteritems():
+    for port_name, data in off_data.items():
         port = port_sample
 
-        port["name"]     = port_name
-        port["location"] = data["location"]
+        port_name_split = port_name.split(',')
+        name    = port_name_split[0].lstrip().rstrip()
+        country = port_name_split[-1].lstrip().rstrip()
 
-        for dest in data["destinations"]
+        cntr_id, cntr_list = add_country(country, cntr_id, cntr_list)
+
+        port["_id"]        = port_id
+        port["name"]       = name
+        port["country_id"] = find_country(country, cntr_list)
+        port["location"]   = data["location"]
+
+        port_list.append(port.copy())
+
+        port_id += 1
+
+    for port_name, data in off_data.items():
+
+        port_name_split = port_name.split(',')
+        name = port_name_split[0].lstrip().rstrip()
+
+        # print(data["destinations"])
+
+        if isinstance(data["destinations"], str):
+            continue
+
+        for destination, distance in data["destinations"].items():
+            d = dest_sample
+            dest_name = destination.lstrip().rstrip().rsplit(' ', 1)[0]
+            d["_id"]         = dest_id
+            d["departure"]   = find_port(name, port_list)
+            if find_port(dest_name, port_list) == -1:
+                continue
+            d["destination"] = find_port(dest_name, port_list)
+            d["distance"]    = distance
+
+            dest_list.append(d.copy())
+
+            dest_id += 1
+
+    list_to_json(port_list, ports)
+    print("Ports added:        ", len(port_list))
+    list_to_json(cntr_list, cntrs)
+    print("Countries added:    ", len(cntr_list))
+    list_to_json(dest_list, dests)
+    print("Destinations added: ", len(dest_list))
+
+    return 0
+
+
+gen_ports()
--- fill_db/utils.py
+++ fill_db/utils.py
@@ -1,20 +1,24 @@
-def json_to_dict(jsonPath):
+def json_to_dict(json_path):
     '''
         Reads JSON file and returns a dict
     '''
     from json import loads
 
-    with open(jsonPath) as raw:
+    with open(json_path) as raw:
         jdict = loads(raw.read())
     return jdict
 
 
-def dict_to_json(jdict, jsonPath):
+def dict_to_json(jdict, json_path):
     '''
         Writes dict as JSON to file
     '''
     from json import dump
 
-    with open(jsonPath, 'w') as raw:
-        dump(jdict, raw, indent=4, sort_keys=False)
+    with open(json_path, 'w') as raw:
+        dump(jdict, raw, indent=4, sort_keys=False, ensure_ascii=False)
     return
+
+
+def list_to_json(jlist, json_path):
+    return dict_to_json(jlist, json_path)
